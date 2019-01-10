398214c0e8887dc4b181ed7c6d61776dc4e86283
terru <agustinterruzzi@gmail.com>
2019-01-10 12:15:44 -0300

    ECMA6 POO more concepts

--- ECMA2015/Maps.js
+++ ECMA2015/Maps.js
@@ -17,6 +17,21 @@ console.log(`Articuno es nivel: ${equipo_pokemon.get("Articuno")}`);
 console.log("Atrape un Mewtwo?", equipo_pokemon.has("Mewtwo"));
 //a diferencia de un {} con el que se podria hacer el equipo
 //toString no existe en el map
-console.log(equipo_pokemon.has("toString"));// → false
+console.log(equipo_pokemon.has("toString")); // → false
 
 //SETS
+/**
+ * Un set acumula colecciones de objetos, igual que un map.
+ * No asocia valores a una clave, siemplemente chequea la unicidad de
+ * los elementos que posee.
+ * Si el elemento ya fue agregado, no se vuelve a agregar 
+ * metodos add, has,entries, values 
+ * propiedad size. 
+ * @see https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Set/size
+ */
+var mySet = new Set();
+mySet.add(1);
+mySet.add(5);
+mySet.add("un texto")
+
+mySet.size; //3
--- POO/ECMA6Style.js
+++ POO/ECMA6Style.js
@@ -18,10 +18,14 @@ class Pokemon {
   }
 
   attack() {
-    console.log('Pokemon: ' + this.especie + ' realizo un ataque');
+    console.log("Pokemon: " + this.especie + " realizo un ataque");
   }
   pokedex() {
-      console.log('Pokemon de tipo: ' + this.tipo);
+    console.log("Pokemon de tipo: " + this.tipo);
+  }
+
+  static evolve() {
+    console.log("Pokemon is evolving....");
   }
 }
 
@@ -40,7 +44,37 @@ dragonite.pokedex();
  * Se puede reescribir cualquier mensaje o propiedad desde el prototype de la clase4
  * incluso los de la clase object
  */
-Pokemon.prototype.toString = function () {
-  console.log('Esto es un pokeon real');
+Pokemon.prototype.toString = function() {
+  console.log("Esto es un pokeon real");
 };
 console.log(Pokemon.toString());
+
+//Metodos estaticos
+//asi como en ECMA5, los metodos estaticos definen metodos de clase
+//este es el caso del metodo evolve de la clase pokemon
+Pokemon.evolve();
+
+/**
+ * Herencia
+ * Javascript permite herencia mediante la copia y agregado de metodos/propiedades
+ * a traves del uso de los prototypos de las distintas clases
+ * Herencia es un concepto clave en POO, da la posibilidad de reutilizar el código
+ * ya construido y adecuarlo a nuevas situaciones.
+ */
+class Tigo_Dragon extends Pokemon {
+  constructor(especie, nombre) {
+    //palabra clave para acceder a propiedades y metodos de la superclase
+    super("Dragon", especie, nombre);
+  }
+  attack() {
+    console.log("El pokemon: " + this.especie + " ataca con furia dragon");
+  }
+}
+
+const dratini = new Tigo_Dragon("Dratini", "chimuelo");
+dratini.pokedex(); //queda igual
+dratini.attack(); //subclase
+
+//instanceof operator, para chequear si un objeto pertenece o no a una clase 
+console.log(dratini instanceof Pokemon); //true
+console.log(dratini instanceof Tigo_Dragon); //true
